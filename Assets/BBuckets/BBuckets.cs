3da6a5409e1f24ee2208781a8deed20dbb392163
Russo-ow <e10benga@gmail.com>
2020-12-27 15:22:02 -0600

    Sound Effects in, Starting on Script

---
+++ Assets/BBuckets/BBuckets.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BBuckets {
+    public class BBuckets : MonoBehaviour {
+        public Text UIText;
+        public string startText;
+        public string winText;
+
+        private void Start() {
+            UIText.text = startText;
+            MinigameManager.Instance.minigame.gameWin = false;
+        }
+
+        private void Update() {
+            if(Input.GetButtonDown("Space")) {
+                if(!MinigameManager.Instance.minigame.gameWin) {
+                    MinigameManager.Instance.minigame.gameWin = true;
+                    UIText.text = winText;
+                    MinigameManager.Instance.PlaySound("win");
+                }
+            }
+        }
+    }
+}
