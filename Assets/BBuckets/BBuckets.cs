2e2d95064cd9b80d526b3996662e84a3fe49141a
Russo-ow <e10benga@gmail.com>
2020-12-27 16:50:05 -0600

    Cow Movement with placeholder sprite

--- Assets/BBuckets/BBuckets.cs
+++ Assets/BBuckets/BBuckets.cs
@@ -1,27 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace BBuckets {
     public class BBuckets : MonoBehaviour {
-        public Text UIText;
-        public string startText;
-        public string winText;
 
         private void Start() {
-            UIText.text = startText;
-            MinigameManager.Instance.minigame.gameWin = false;
+            MinigameManager.Instance.minigame.gameWin = true;
+            StartCoroutine(SawsSound());
         }
 
         private void Update() {
             if(Input.GetButtonDown("Space")) {
                 if(!MinigameManager.Instance.minigame.gameWin) {
                     MinigameManager.Instance.minigame.gameWin = true;
-                    UIText.text = winText;
-                    MinigameManager.Instance.PlaySound("win");
+                    MinigameManager.Instance.PlaySound("moo");
                 }
             }
+        }
+
+        IEnumerator SawsSound() {
+            yield return new WaitForSeconds(.5f);
+            MinigameManager.Instance.PlaySound("saws");
         }
     }
 }
---
+++ Assets/BBuckets/Cow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BBuckets {
+    public class Cow : MonoBehaviour {
+        public float speed;
+        Rigidbody2D rb;
+
+        void Start() {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        void Update() {
+            rb.velocity = Vector2.right * Input.GetAxis("Horizontal") * speed;
+        }
+    }
+}
