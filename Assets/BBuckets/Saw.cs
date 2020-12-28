2a6246852ec40d40a6a46ea2ad2ba82b2aeb97c3
MindyV21 <mmv0321@gmail.com>
2020-12-27 18:32:28 -0600

    saw

---
+++ Assets/BBuckets/Saw.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BBuckets {
+    public class Saw : MonoBehaviour {
+        Rigidbody2D rb;
+        public float gravity;
+
+        void Start() {
+            rb = GetComponent<Rigidbody2D>();
+            rb.gravityScale = 0;
+        }
+
+        void Update() {
+            if (Input.GetButtonDown("Space")) {
+                rb.gravityScale = gravity;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            GameObject cow = other.gameObject;
+            rb.gravityScale = 0;
+            cow.GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
+    }
+}
