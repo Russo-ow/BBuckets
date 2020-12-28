04c53686e93a123fe47eba20393f37bc70692d44
Russo-ow <e10benga@gmail.com>
2020-12-27 19:27:47 -0600

    Sawblade Revving with new sprite

--- Assets/BBuckets/Saw.cs
+++ Assets/BBuckets/Saw.cs
@@ -6,22 +6,35 @@ namespace BBuckets {
     public class Saw : MonoBehaviour {
         Rigidbody2D rb;
         public float gravity;
+        float speed = 0;
 
         void Start() {
             rb = GetComponent<Rigidbody2D>();
             rb.gravityScale = 0;
+            StartCoroutine(SawRevving());
         }
 
         void Update() {
             if (Input.GetButtonDown("Space")) {
                 rb.gravityScale = gravity;
             }
+            transform.Rotate(Vector3.forward * speed);
         }
 
         private void OnTriggerEnter2D(Collider2D other) {
             GameObject cow = other.gameObject;
             BBuckets.instance.Lose();
-            Debug.Log("....");
+        }
+
+        IEnumerator SawRevving() {
+            float timer = 0;
+            yield return new WaitForSeconds(.5f);
+            while(speed<=110) {
+                timer += Time.deltaTime;
+                speed = 27.5f * (timer * timer);
+                yield return new WaitForEndOfFrame();
+            }
+            speed = 110;
         }
     }
 }
